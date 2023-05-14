7509eeeb8cf457f0d94b387f98771765f7cb7d88
Sam Teel <sjkteel@hotmail.com>
2023-05-14 19:06:41 -0400

    rewrote script

--- script.js
+++ script.js
@@ -1,76 +1,65 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var special = ["!","'","#","$"];
-
-
 
 // Write password to the #password input
 function writePassword() {
-  //Here we generate the password
-  var password = generatePassword();{ 
-    var myArray =[];                                  
-      var passLength = window.prompt("How many characters long would you like in the password?");
-      var passSymbol = window.prompt("Include Special Symbols?");
-      var passUppers = window.prompt("Include Uppercase?");
-      var passLowers = window.prompt("Include Lowers?");
-      var passNum = window.prompt("Include Numbers?");
-
-      if (passLength > 8 && passLength < 128) {
-        for (var i =0;i<passLength;i++){
-              //iterating through the length asked for and randomly picking from alphabet, myArray is filled with random letters.
-              var passPicker = Math.floor(Math.random()*26);
-              myArray[i]=alphabet[passPicker];
-            }
-      }
-      else if (passSymbol == "yes"){
-        for (var i =0;i<passLength;i++){
-          //iterating through the length asked for and randomly picking from alphabet, myArray is filled with random letters.
-          var passPicker = Math.floor(Math.random()*4);
-          myArray[i]=special[passPicker];
-        }
-      }
-      else if (passUppers=="yes"){
-        for (var i =0;i<passLength;i++){
-          //iterating through the length asked for and randomly picking from alphabet, myArray is filled with random letters.
-          var passPicker = Math.floor(Math.random()*26);
-          myArray[i]=alphabet[passPicker];
-          function isOdd(){
-            return i%2;
-          }
-          if (isOdd = 1){
-            myArray[i].toUppercase();
-          }
-        }
-      }
-      else if (passLowers == "yes"){
-        for (var i =0;i<passLength;i++){
-          //iterating through the length asked for and randomly picking from alphabet, myArray is filled with random letters.
-          var passPicker = Math.floor(Math.random()*26);
-          myArray[i]=alphabet[passPicker];
-          myArray[i].toUpperCase();
-          function isOdd(){
-            return i%2;
-          }
-          if (isOdd = 1){
-            myArray[i].toLowercase();
-          }
-        }
-      }
-      else if (passNum=="yes"){
-        for (var i =0;i<passLength;i++){
-          //iterating through the length asked for and randomly picking from alphabet, myArray is filled with random letters.
-          var passPicker = Math.floor(Math.random()*9);
-          myArray[i]=passPicker;
-      }
-   }
-   else {
-    console.log("woops nothing went...");
-   }
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  passwordText.value = password;  
-  return myArray;
+  generatePassword(){
+    var length = window.prompt ("How long do you want the password?");
+    var upChoice = window.prompt("UpperCase?");
+    var lowChoice = window.prompt("LowerCase?");
+    var numChoice = window.prompt("numbers?");
+    var symbolChoice = window.prompt("symbols?");
+    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lowerCase= "abcdefghijklmnopqrstuvwxyz";
+    var number = "123456789";
+    var symbol = "@#$%^&*()";
+    var myPass = "";
+     //picks a random number which then matches a value in the string of that variable
+    myPass += upperCase[Math.floor(Math.random()*upperCase.length)];
+    myPass += lowerCase[Math.floor(Math.random()*lowerCase.length)];
+    myPass += number[Math.floor(Math.random()*number.length)];
+    myPass += symbol[Math.floor(Math.random()*symbol.length)];
+
+    if (length >= 8 && length <= 128 && !upChoice && !lowChoice && !numChoice && !symbolChoice){
+      //gens the simplest password
+      for(var i =0; i>length;i++){
+        myPass += lowerCase[Math.floor(Math.random()*lowerCase.length)];
+      }
+    }
+    else if (length >= 8 && length <= 128 && upChoice && !lowChoice && !numChoice && !symbolChoice){
+    for(var i =0; i>length;i++){
+      myPass += upperCase[Math.floor(Math.random()*upperCase.length)];
+    }
+  }
+    else if (length >= 8 && length <= 128 && upChoice && lowChoice && !numChoice && !symbolChoice){
+      for(var i =0; i>length;i++){
+        myPass += upperCase[Math.floor(Math.random()*upperCase.length)];
+        myPass += lowerCase[Math.floor(Math.random()*lowerCase.length)];
+      }
+    }
+    else if (length >= 8 && length <= 128 && upChoice && lowChoice && numChoice && !symbolChoice){
+      for(var i =0; i>length;i++){
+        myPass += upperCase[Math.floor(Math.random()*upperCase.length)];
+        myPass += lowerCase[Math.floor(Math.random()*lowerCase.length)];
+        myPass += number[Math.floor(Math.random()*number.length)];
+      }
+    }
+    else if (length >= 8 && length <= 128 && upChoice && lowChoice && numChoice && symbolChoice){
+      for(var i =0; i>length;i++){
+        myPass += upperCase[Math.floor(Math.random()*upperCase.length)];
+        myPass += lowerCase[Math.floor(Math.random()*lowerCase.length)];
+        myPass += number[Math.floor(Math.random()*number.length)];
+        myPass += symbol[Math.floor(Math.random()*symbol.length)];
+      }
+
+  }
+  else {}
+
+  passwordText.value = password;
+
 }
-}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
