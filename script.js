e4f250997ef395cbbfa6e21800a124e6454d5a42
Sam Teel <sjkteel@hotmail.com>
2023-05-14 19:59:57 -0400

    added if elses

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var prompt = window.prompt("Hello???");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  generatePassword(){
+  function generatePassword(){
     var length = window.prompt ("How long do you want the password?");
     var upChoice = window.prompt("UpperCase?");
     var lowChoice = window.prompt("LowerCase?");
@@ -17,10 +17,10 @@ function writePassword() {
     var symbol = "@#$%^&*()";
     var myPass = "";
      //picks a random number which then matches a value in the string of that variable
-    myPass += upperCase[Math.floor(Math.random()*upperCase.length)];
-    myPass += lowerCase[Math.floor(Math.random()*lowerCase.length)];
-    myPass += number[Math.floor(Math.random()*number.length)];
-    myPass += symbol[Math.floor(Math.random()*symbol.length)];
+    // myPass += upperCase[Math.floor(Math.random()*upperCase.length)];
+    // myPass += lowerCase[Math.floor(Math.random()*lowerCase.length)];
+    // myPass += number[Math.floor(Math.random()*number.length)];
+    // myPass += symbol[Math.floor(Math.random()*symbol.length)];
 
     if (length >= 8 && length <= 128 && !upChoice && !lowChoice && !numChoice && !symbolChoice){
       //gens the simplest password
@@ -29,29 +29,30 @@ function writePassword() {
       }
     }
     else if (length >= 8 && length <= 128 && upChoice && !lowChoice && !numChoice && !symbolChoice){
+      //gens an all uppercase pass
     for(var i =0; i>length;i++){
       myPass += upperCase[Math.floor(Math.random()*upperCase.length)];
     }
   }
     else if (length >= 8 && length <= 128 && upChoice && lowChoice && !numChoice && !symbolChoice){
+      //gens an upper and lower case pass
+      var uppAndLow = upperCase + lowerCase;
       for(var i =0; i>length;i++){
-        myPass += upperCase[Math.floor(Math.random()*upperCase.length)];
-        myPass += lowerCase[Math.floor(Math.random()*lowerCase.length)];
+        myPass += uppAndLow[Math.floor(Math.random()*uppAndLow.length)];
       }
     }
     else if (length >= 8 && length <= 128 && upChoice && lowChoice && numChoice && !symbolChoice){
+      // gens an upper lower and number pass
+      var uppLowNum = upperCase + lowerCase + number;
       for(var i =0; i>length;i++){
-        myPass += upperCase[Math.floor(Math.random()*upperCase.length)];
-        myPass += lowerCase[Math.floor(Math.random()*lowerCase.length)];
-        myPass += number[Math.floor(Math.random()*number.length)];
+        myPass += uppLowNum[Math.floor(Math.random()*uppLowNum.length)];
       }
     }
     else if (length >= 8 && length <= 128 && upChoice && lowChoice && numChoice && symbolChoice){
+      //gens a pass with all options
+      var allOpt = uppLowNum + symbol;
       for(var i =0; i>length;i++){
-        myPass += upperCase[Math.floor(Math.random()*upperCase.length)];
-        myPass += lowerCase[Math.floor(Math.random()*lowerCase.length)];
-        myPass += number[Math.floor(Math.random()*number.length)];
-        myPass += symbol[Math.floor(Math.random()*symbol.length)];
+        myPass += allOpt[Math.floor(Math.random()*allOpt.length)];
       }
 
   }
@@ -59,7 +60,8 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
